6595caf385a8b845ee1079ee7671650941a8a195
Johan Cortes <joco1204@gmail.com>
2024-10-31 06:31:51 -0500

    modificacion ruta imagenes

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, send_from_directory, abort
 
 imagenes_pb = Blueprint('imagenes', __name__)
 
-@imagenes_pb.route('/api/imagenes/<filename>', methods=['GET'])
+@imagenes_pb.route('api/imagenes/<filename>', methods=['GET'])
 def obtener_imagen(filename):
     try:
         return send_from_directory('static/imagenes', filename)
